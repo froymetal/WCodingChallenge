1a9184bbac8801fc9452d0449ead85ab7f625366
Froy <froylan.almeida@gmail.com>
2021-08-31 15:02:01 -0500

    Initial Commit

---
+++ WCodingChallenge/WCodingChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WCodingChallengeApp.swift
+//  WCodingChallenge
+//
+//  Created by Field Employee on 8/31/21.
+//
+
+import SwiftUI
+
+@main
+struct WCodingChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
